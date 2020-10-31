c53e20c56258fb2e4d8938e7d840f56f5d7dbb06
Bohdan Polataiko <bohdan.polataiko@gmail.com>
2020-10-31 14:13:39 +0200

    update global style

--- src/global-style.ts
+++ src/global-style.ts
@@ -16,6 +16,17 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  /* html {
+    font-family: sans-serif;
+    font-size: 100%;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  } */
+
+  /* html,
+  body {
+    height: 100%;
+  } */
+
   body {
     margin: 0;
     font-family: 'Roboto Regular', sans-serif;
@@ -25,4 +36,21 @@ export default createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+
+  /* img {
+    image-rendering: -webkit-optimize-contrast;
+    -ms-interpolation-mode: bicubic;
+  } */
+
+  /* Holy Grail Layout */
+
+  /* #app {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  #app > main {
+    display: flex;
+    flex: 1;
+  } */
 `;
