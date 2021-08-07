2b256c01f083d1f8bf34605cdc39c2822eb88910
Bohdan Polataiko <bohdan.polataiko@gmail.com>
2021-08-07 16:51:36 +0300

    removed unnecessary packages

--- babel.config.js
+++ babel.config.js
@@ -1,49 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable global-require */
-const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const PACKAGE_JSON = require('./package.json');
 
 module.exports = {
-  presets: [
-    [
-      '@babel/env',
-      {
-        // Allow importing core-js in entrypoint and use browserlist to select polyfills
-        useBuiltIns: 'entry',
-        // Set the corejs version we are using to avoid warnings in console
-        // This will need to change once we upgrade to corejs@3
-        corejs: 3,
-        // Exclude transforms that make all code slower
-        exclude: ['transform-typeof-symbol'],
-      },
-    ],
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
-  plugins: [
-    // Polyfills the runtime needed for async/await, generators, and friends
-    // https://babeljs.io/docs/en/babel-plugin-transform-runtime
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: false,
-        helpers: false,
-        // By default, babel assumes babel/runtime version 7.0.0-beta.0,
-        // explicitly resolving to match the provided helper functions.
-        // https://github.com/babel/babel/issues/10261
-        version: require('@babel/runtime/package.json').version,
-        regenerator: true,
-        // https://babeljs.io/docs/en/babel-plugin-transform-runtime#useesmodules
-        // We should turn this on once the lowest version of Node LTS
-        // supports ES Modules.
-        useESModules: false,
-        // Undocumented option that lets us encapsulate our runtime, ensuring
-        // the correct version is used
-        // https://github.com/babel/babel/blob/090c364a90fe73d36a30707fc612ce037bdbbb24/packages/babel-plugin-transform-runtime/src/index.js#L35-L42
-        absoluteRuntime: path.dirname(require.resolve('@babel/runtime/package.json')),
-      },
-    ],
-  ],
+  presets: ['@babel/preset-typescript', '@babel/preset-react'],
   env: {
     development: {
       plugins: [['babel-plugin-styled-components', { displayName: true, namespace: PACKAGE_JSON.name }]],
